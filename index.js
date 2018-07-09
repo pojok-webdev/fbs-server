3a8d2bd81860b8251ff2a423c89576a4058a552e
pujie <puji@gmail.com>
2018-07-09 14:54:09 +0700

    add pics crud

--- index.js
+++ index.js
@@ -28,15 +28,45 @@ app.get('/getfb/:nofb',(req,res)=>{
     })
 })
 app.post('/savefb',(req,res) => {
-    db.executeQuery(query.saveFb(req.bodyParser),result=>{
+    db.executeQuery(query.saveFb(req.body),result=>{
         console.log("savefb",result)
         res.send(result)
     })
 })
 app.post('/updatefb',(req,res) => {
-    db.executeQuery(query.saveFb(req.req.bodyParser),result=>{
-        console.log("savefb",result)
+    console.log("Body Parser",req.body)
+    console.log("Query",query.updateFb(req.body))
+    db.executeQuery(query.updateFb(req.body),result=>{
+        console.log("updatefb",result)
         res.send(result)
     })
 })
+
+app.get('/getpics/:nofb',(req,res)=>{
+    db.executeQuery(query.getPics({nofb:req.params.nofb}),result=>{
+        console.log("getpic",result)
+        res.send(result)
+    })
+})
+app.get('/getpic/:id',(req,res)=>{
+    db.executeQuery(query.getFb({id:req.params.id}),result=>{
+        console.log("getpic",result)
+        res.send(result)
+    })
+})
+app.post('/savepic',(req,res) => {
+    db.executeQuery(query.savePic(req.body),result=>{
+        console.log("savepic",result)
+        res.send(result)
+    })
+})
+app.post('/updatepic',(req,res) => {
+    console.log("Body Parser",req.body)
+    console.log("Query",query.updatePic(req.body))
+    db.executeQuery(query.updatePic(req.body),result=>{
+        console.log("updatepic",result)
+        res.send(result)
+    })
+})
+
 app.listen(process.env.PORT || 2000)
--- js/queries.js
+++ js/queries.js
@@ -4,25 +4,56 @@ getFbs = () => {
 }
 getFb = obj => {
     sql = 'select * from fbs '
-    sql+= 'where nofb = ' + obj.nofb + ' '
+    sql+= 'where nofb = "' + obj.nofb + '" '
     return sql
 }
 saveFb = obj => {
     sql = 'insert into fbs '
-    sql+= '(nofb,client_id,client_site_id,name,businesstype,siup,npwp,address,city,telp,fax,activationdate,period1,period2,services)'
+    sql+= '(nofb,client_id,name,businesstype,siup,npwp,address,city,telp,fax,activationdate,period1,period2,services)'
     sql+= 'values '
-    sql+= '("'+obj.nofb+'","'+obj.client_id+'","'+obj.client_site_id+'","'+obj.name+'","'+obj.businesstype+'","'+obj.siup+'","'+obj.npwp+'","'+obj.address+'","'+obj.city+'","'+obj.telp+'","'+obj.fax+'","'+obj.activationdate+'","'+obj.period1+'","'+obj.period2+'","'+obj.services+'")'
+    sql+= '("'+obj.nofb+'","'+obj.client_id+'","'+obj.name+'","'+obj.businesstype+'","'+obj.siup+'","'+obj.npwp+'","'+obj.address+'","'+obj.city+'","'+obj.telp+'","'+obj.fax+'","'+obj.activationdate+'","'+obj.period1+'","'+obj.period2+'","'+obj.services+'")'
     return sql
 }
 updateFb = obj => {
     sql = 'update fbs '
     sql+= 'set '
-    sql+= 'nofb="'+obj.nofb+'",client_id="'+obj.client_id+'",client_site_id="'+obj.client_site_id+'",name="'+obj.name+'",businesstype="'+obj.businesstype+'",siup="'+obj.siup+'",npwp="'+obj.npwp+'",address="'+obj.address+'",city="'+obj.city+'",telp="'+obj.telp+'",fax="'+obj.fax+'",activationdate="'+obj.activationdate+'",period1="'+obj.period1+'",period2="'+obj.period2+'",services="'+obj.services+'"'
+    sql+= 'nofb="'+obj.nofb+'",client_id="'+obj.client_id+'",name="'+obj.name+'",businesstype="'+obj.businesstype+'",siup="'+obj.siup+'",npwp="'+obj.npwp+'",address="'+obj.address+'",city="'+obj.city+'",telp="'+obj.telp+'",fax="'+obj.fax+'",activationdate="'+obj.activationdate+'",period1="'+obj.period1+'",period2="'+obj.period2+'",services="'+obj.services+'"'
+    sql+= 'where nofb="'+obj.nofb+'"'
     return sql
 }
+
+getPics = (obj) => {
+    sql = 'select * from fbpics '
+    sql+= 'where nofb = "' + obj.nofb + '" '
+    return sql
+}
+getPic = obj => {
+    sql = 'select * from fbpics '
+    sql+= 'where nofb = "' + obj.id + '" '
+    return sql
+}
+savePic = obj => {
+    sql = 'insert into fbpics '
+    sql+= '(name,nofb,role,position,idnum,phone,hp,email)'
+    sql+= 'values '
+    sql+= '("'+obj.name+'","'+obj.nofb+'","'+obj.role+'","'+obj.position+'","'+obj.idnum+'","'+obj.phone+'","'+obj.hp+'","'+obj.email+'")'
+    return sql
+}
+updatePic = obj => {
+    sql = 'update fbpics '
+    sql+= 'set '
+    sql+= 'nofb="'+obj.nofb+'",name="'+obj.name+'",role="'+obj.role+'",position="'+obj.position+'",idnum="'+obj.idnum+'",phone="'+obj.phone+'",hp="'+obj.hp+'",email="'+obj.email+'"'
+    sql+= 'where nofb="'+obj.nofb+'"'
+    return sql
+}
+
 module.exports = {
     getFbs : getFbs,
     getFb : getFb,
     saveFb : saveFb,
-    updateFb : updateFb
+    updateFb : updateFb,
+    getPics : getPics,
+    getPic : getPic,
+    savePic : savePic,
+    updatePic : updatePic
 }
